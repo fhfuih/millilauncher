d845e895d9657a40066776b10b0c86d05d96f258
fhfuih <sam.zyhuang@outlook.com>
2017-04-10 23:10:02 +0800

    Signed-off-by: fhfuih <sam.zyhuang@outlook.com>

--- api/launcher.py
+++ api/launcher.py
@@ -5,11 +5,15 @@ import system_info as si
 
 class Launcher(object):
     'Launcher Object'
-    template_script = '{javaw} {extra} -Xmx{maxmem}M -Djava.library.path={native} -cp {libs} {main} {mcargs}'
+    template_script = '{javaw} {extra} -Xmx{maxmem}M -Djava.library.path={natives} -cp {libs} {main} {mcargs}'
     def __init__(self, mcdir, javadir='javaw'):
         self.mcdir = mcdir if mcdir else si.get_default_minecraft_directory()
         if not self.mcdir:
             raise FileNotFoundError('Invalid /.Minecraft/ directory.')
+        self.libraries_directory = os.path.join(self.mcdir, 'libraries')
+        self.assets_directory = os.path.join(self.mcdir, 'assets')
+        self.version_directory = None
+        self.natives_directory = None
         os.chdir(self.mcdir)
         self.versions = vd.VersionsDict()
 
@@ -21,6 +25,8 @@ class Launcher(object):
         version = self.versions.get(version_id)
         if not version:
             raise AttributeError('Version of this id does not exist.')
+        self._update_directories(version_id)
+
         jar = os.path.join(self.mcdir, 'versions', version.jar, version.jar + '.jar')
         libraries = ';'.join([lib.path for lib in version.libraries if lib.allow]) + ';' + jar
         mcargs = version.minecraft_arguments.format(auth_player_name=username,
@@ -35,15 +41,18 @@ class Launcher(object):
         return Launcher.template_script.format(javaw='javaw',
                                                extra=self.extra_argument,
                                                maxmem=maxmem,
-                                               native='',
+                                               natives='',
                                                libs=libraries,
                                                main=version.main_class,
                                                mcargs=mcargs)
+    def _update_directories(self, version_id):
+        self.version_directory = os.path.join(self.mcdir, 'versions', version_id)
+        self.natives_directory = os.path.join(self.version_directory, self.version_directory + '-natives')
 
     @staticmethod
-    def _extract_native(jar_path):
-        'Extract native files from a jar file'
-        with ZipFile(jar_path) as zip:
+    def _extract_natives(file, exclude):
+        'Extract natives files from a jar file'
+        with ZipFile(file) as zip:
             pass
 
 if __name__ == '__main__':
